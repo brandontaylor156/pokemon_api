104953d13dbff073afaef74f15046c7e90f8eb42
brandontaylor156 <brandon.david4@outlook.com>
2022-09-03 01:27:19 -0700

    axios version

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = React.useState([])
+  const [buttonClicked, setButtonClicked] = React.useState(false);
 
-  const displayPokemon = async () => {
-    try {
-      const data = await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')).json()
-      setPokemon(data.results)
-    } catch (err) {
-      console.log(err.message)
-    }
+  React.useEffect(()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+        .then(response=>{setPokemon(response.data.results)})
+    }, []); 
+
+  const handleToggle = () => {
+    setButtonClicked(!buttonClicked);
   }
 
   return (
     <div>
-        <button onClick={displayPokemon}>Fetch Pokemon</button>
+        <button onClick={handleToggle}>Fetch Pokemon</button>
         <div>
-            {pokemon.length > 0 && pokemon.map((poke, index)=>{
+            {buttonClicked && pokemon.map((poke, index)=>{
                 return (<div key={index}>{poke.name}</div>)
             })}
         </div>
